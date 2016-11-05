bf3e5284a0a6b2faea13944d032ce03bb4af3547
anoopvkm <anoopvkm@gmail.com>
2016-11-05 09:37:46 -0400

    Randomized test cases

--- parser/testcases.py
+++ parser/testcases.py
@@ -3,70 +3,77 @@ import fnmatch
 import os
 import itertools
 from nltk.tokenize import wordpunct_tokenize
-
+import numpy as np
+import random
 def gen_sens(eng,m,n):
-    sample = ['Tobacco Industry Responsibility Bill',
-            "Second Reading-Points of Order-Speaker's Ruling",
-            'Referred to Committee',
-            'Transport and Communication',
-            'Motion to Authorize Committee to Meet During Sittings of the Senate-Order Withdrawn',
-            'The Senate',
-            'Lack of Accessibility for Disabled to Facilities-Inquiry-Debate Concluded',
-            'Inter-Provincial Relations',
-            'Applicability of Subpoenas Issued in Relation to Commissions of Inquiry-Motion-Debate Adjourned',
-            'Aboriginal Peoples',
-            'First Nations Government Bill-Committee Authorized to Apply Materials and Evidence Gathered on Examination of Previous Bills to Study of Current Bill',
-            'Business of the Senate']
     res = []
     words = wordpunct_tokenize(eng)
-    for i in range(0,m):
-        for j in range(1,len(words)):
-            temp = words[j]
-            words[j] = words[j-1]
-            words[j-1] = temp
+    for i in range(0,m-4):
+        perm = []
+        
+        perm = list(np.random.permutation(words))
         ans = ''
-        for j in range(0,len(words)):
-            ans = ans+words[j]
+        for j in range(0,len(perm)):
+            ans = ans+" "+perm[j]
         res.append(ans)
 
-    for i in range(0,n):
+    with open('../sample.txt') as f:
+        sample = f.read().splitlines()
+
+    s = random.randint(0,len(sample)-n-1)
+    for i in range(s,s+n):
         res.append(sample[i])
+
+    idx = random.randint(0,len(words)-1)
+    ans = ''
+    for j in range(0,len(words)):
+        if j != idx:
+            ans = ans+words[j]
+    res.append(ans)
+    for i in range(0,3):
+        idx = random.randint(0,len(words)-1)
+        perm = list(np.random.permutation(words))
+        ans = ''
+        for j in range(0,len(perm)):
+            if j != idx:
+                ans = ans+perm[j]
+        res.append(ans)
+        
     return res
 
 
-def gen_test_cases(m,n):
+def gen_test_cases(m,n,fname,ename,rname):
     print "Generating test cases "
     count = 0
-    resf = open('../testcase.txt','w')
-
+    rese = open(ename,'w')
+    resf = open(rname,'w')
     new_dict = dict()
-    for file in os.listdir(filenames.HOUSE_TRAINING):
+    for file in os.listdir(fname):
         
-        if count > 100:
-            break;
+        if count >= 1000:
+            break
         if fnmatch.fnmatch(file,'*.f'):
+            print file
             efile = file[:-2]+'.e'
-            with open(filenames.HOUSE_TRAINING+file) as f :
+            with open(fname+file) as f :
                 french = f.read().splitlines()
-            with open(filenames.HOUSE_TRAINING+efile) as f:
+            with open(fname+efile) as f:
                 english = f.read().splitlines()
-
 
             for i in range(0, len(french)):
                 resf.write(french[i]+'\n')
-                resf.write(english[i]+'\n')
                 res = gen_sens(english[i],m,n)
-                for j in range(0,len(res)):
-                    resf.write(res[j]+'\n')
                 res.append(english[i])
+                for j in range(0,len(res)):
+                    rese.write(res[j]+'\n')
                 new_dict[french[i]] = res
-                if(count >= 100):
+                if(count >= 1000):
                     break
-                count += 1
+                count = count + 1
 
     return new_dict
 
 def main():
-    gen_test_cases(2,3)
-
+    gen_test_cases(20,20,filenames.HOUSE_TRAINING,'../testcasese.txt','../testcasesf.txt')
+    gen_test_cases(20,20,filenames.SENATE_TRAINING,'../testcasesse.txt','../testcasessf.txt')
 main()
